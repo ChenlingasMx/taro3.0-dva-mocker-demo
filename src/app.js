72a3c43e5d9c4ccd9abc4843c33ee673eb583b05
Frinter <84749353@qq.com>
2020-10-15 17:31:12 +0800

    使用dva

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import dva from './dva';
-import models from './models';
-import './app.scss'
-const dvaApp = dva.createApp({
-    initialState: {},
-    models,
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import dva from './dva';
+import models from './models';
+import './app.scss'
+const dvaApp = dva.createApp({
+    initialState: {},
+    models,
 });
 
-const store = dvaApp.getStore();
-class App extends Component {
+const store = dvaApp.getStore();
+class App extends Component {
 
-    componentDidMount() { }
+    componentDidMount() { }
 
-    componentDidShow() { }
+    componentDidShow() { }
 
-    componentDidHide() { }
+    componentDidHide() { }
 
-    componentDidCatchError() { }
+    componentDidCatchError() { }
 
-    // this.props.children 是将要会渲染的页面
+    // this.props.children 是将要会渲染的页面
 
-    render() {
-    console.log(this.props)
-        return (
-            <Provider store={store}>
-                {this.props.children}
-            </Provider>
-        )
-    }
+    render() {
+        return (
+            <Provider store={store}>
+                {this.props.children}
+            </Provider>
+        )
+    }
 }
-
-export default App
+export default App

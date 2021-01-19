a78f4bf7e53246692e22a74cc3ac375481b61060
WX <42308734@qq.com>
2021-01-19 09:58:44 +0800

    fix:eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
   'extends': ['taro/react']
 }
+module.exports={
+  extends: [
+    // ...
+    "plugin:react-hooks/recommended"
+  ],
+  plugins: [
+    // ...
+    "react-hooks"
+  ],
+  rules: {
+    // ...
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
+}
--- src/pages/user/index/Tabs/TabTwo.jsx
+++ src/pages/user/index/Tabs/TabTwo.jsx
@@ -1,9 +1,12 @@
-import React, {} from 'react'
+import React, {useEffect} from 'react'
 import { View,Text} from '@tarojs/components'
 //import { AtTabs, AtTabsPane } from 'taro-ui'
 import {connect} from 'react-redux'
 
-const TabTwo = () => {
+const TabTwo = (props) => {
+    useEffect(()=>{
+      props.dispatch({type: "user/getList"})
+    },[])
     return (
       <View>
         <Text>11111111111</Text>
@@ -11,4 +14,4 @@ const TabTwo = () => {
     )
 }
 
-export default connect(({index})=>({index}))(TabTwo);
+export default connect(({user})=>({user}))(TabTwo);

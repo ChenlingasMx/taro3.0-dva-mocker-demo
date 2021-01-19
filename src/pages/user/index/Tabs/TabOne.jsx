7f8f084e661ce6a03243d9d97d07a1330da2465c
WX <42308734@qq.com>
2021-01-19 10:46:10 +0800

    fix:增加搜索栏

--- src/pages/user/index/Tabs/TabOne.jsx
+++ src/pages/user/index/Tabs/TabOne.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
 import { View } from '@tarojs/components'
-import { AtList, AtListItem } from 'taro-ui'
+import { AtList, AtListItem, AtSearchBar } from 'taro-ui'
 import "taro-ui/dist/style/components/list.scss";
+import "taro-ui/dist/style/components/icon.scss";
+import "taro-ui/dist/style/components/search-bar.scss";
+import "taro-ui/dist/style/components/button.scss";
 import "taro-ui/dist/style/components/icon.scss";
 import { connect } from 'react-redux'
 
 class TabOne extends Component {
+  state = {
+    value: ''
+  }
   componentDidMount() {
-    this.props.dispatch({type: "user/getList"})
+    this.props.dispatch({ type: "user/getList" })
+  }
+  onChange(value) {
+    this.setState({
+      value: value
+    })
   }
   render() {
     const { user: { sourceList } } = this.props
     return (
       <View>
+        <AtSearchBar
+          value={this.state.value}
+          onChange={this.onChange.bind(this)}
+        />
         <AtList>
           {sourceList.map(itm => {
             return (
